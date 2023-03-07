ff61e19a417f0d870c137f5298cbfc59fa30c310
HugoconH07 <Hugo.pineda@fundacion-jala.org>
2023-03-07 07:19:05 -0800

    Class 1: Dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,9 @@
-import pygame
-
 class Dinosaur:
-    def __init__(self):
-        pass
-
-    def update(self):
-        pass
-
-    def draw(self):
-        pass
+   def __init__(self):
+      pass
+   def update(self):
+      pass
+   def draw(self):
+      pass
+   def run(self):
+      pass
